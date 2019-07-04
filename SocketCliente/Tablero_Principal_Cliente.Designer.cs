56e64a19f25621b028ed16f42e37bd09b488f45a
Emiliano Calvacho <39678652+emicalvacho@users.noreply.github.com>
2019-07-04 10:34:13 -0300

    Add files via upload

---
+++ SocketCliente/Tablero_Principal_Cliente.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace SocketCliente
+{
+    partial class Tablero_Principal_Cliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerCheckBuffer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftHand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightHand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFichasPozo)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightHandUP)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftHandUp)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDado1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDado2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMisFichas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFichaAdv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonNum2
+            // 
+            this.buttonNum2.FlatAppearance.BorderSize = 0;
+            this.buttonNum2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum3
+            // 
+            this.buttonNum3.FlatAppearance.BorderSize = 0;
+            this.buttonNum3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum4
+            // 
+            this.buttonNum4.FlatAppearance.BorderSize = 0;
+            this.buttonNum4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum5
+            // 
+            this.buttonNum5.FlatAppearance.BorderSize = 0;
+            this.buttonNum5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum6
+            // 
+            this.buttonNum6.FlatAppearance.BorderSize = 0;
+            this.buttonNum6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum12
+            // 
+            this.buttonNum12.FlatAppearance.BorderSize = 0;
+            this.buttonNum12.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum12.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum11
+            // 
+            this.buttonNum11.FlatAppearance.BorderSize = 0;
+            this.buttonNum11.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum11.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum10
+            // 
+            this.buttonNum10.FlatAppearance.BorderSize = 0;
+            this.buttonNum10.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum10.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum9
+            // 
+            this.buttonNum9.FlatAppearance.BorderSize = 0;
+            this.buttonNum9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonNum8
+            // 
+            this.buttonNum8.FlatAppearance.BorderSize = 0;
+            this.buttonNum8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonNum8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            // 
+            // buttonCubilete
+            // 
+            this.buttonCubilete.FlatAppearance.BorderSize = 0;
+            this.buttonCubilete.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.buttonCubilete.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.buttonCubilete.ForeColor = System.Drawing.Color.Transparent;
+            // 
+            // timerCheckBuffer
+            // 
+            this.timerCheckBuffer.Interval = 1000;
+            this.timerCheckBuffer.Tick += new System.EventHandler(this.timerCheckBuffer_Tick);
+            // 
+            // Tablero_Principal_Cliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Name = "Tablero_Principal_Cliente";
+            this.Text = "TableroCliente";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Tablero_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftHand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightHand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFichasPozo)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRightHandUP)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLeftHandUp)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDado1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDado2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMisFichas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFichaAdv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerCheckBuffer;
+    }
+}
